9a55f0feed557118a6a3345d4e2d07ec997df38b
tat131313 <tatyapleshka@gmail.com>
2018-07-18 15:59:52 +0300

    migration for all tables

---
+++ database/migrations/2018_07_18_125248_create_answer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('question_id');
+            $table->text('answer');
+            $table->boolean('is_correct');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer');
+    }
+}
---
+++ database/migrations/2018_07_18_125626_create_user_answer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_answer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('user_id');
+            $table->text('question_id');
+            $table->text('answer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answer');
+    }
+}
